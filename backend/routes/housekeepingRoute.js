4a928744b40896936aadc083169521044d94f3f5
Zain Raza <107460057+razazain@users.noreply.github.com>
2025-10-05 12:57:24 +0500

    Delete backend directory

--- backend/routes/housekeepingRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getHousekeepingDetails, getHousekeepingById, createHousekeeping, updateHousekeeping, deleteHousekeeping } = require("../controllers/HousekeepingController");
-
-router.route('/').get(getHousekeepingDetails).post(createHousekeeping);
-router.route('/:id').get(getHousekeepingById).put(updateHousekeeping).delete(deleteHousekeeping);
-
-module.exports = router;
